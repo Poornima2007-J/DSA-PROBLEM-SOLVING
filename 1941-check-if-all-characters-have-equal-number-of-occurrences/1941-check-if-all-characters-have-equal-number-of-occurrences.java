ba4f284d74512f07aeab6419c490ee5f2c1ca38d
POORNIMA J <poornijayaprakash5@gmail.com>
2026-07-23 10:22:37 +0530

    Time: 12 ms (7.67%), Space: 43.5 MB (38.06%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -5,16 +5,30 @@ class Solution {
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
 
-            if(map.containsKey(ch))
-            {
-                map.put(ch,map.get(ch)+1);
-            }
-            else
-            {
-                map.put(ch,1);
-            }     
-        
+            // if(map.containsKey(ch))
+            // {
+                // map.put(ch,map.get(ch)+1);
+                int newVal=map.getOrDefault(s.charAt(i),0)+1;
+                map.put(s.charAt(i),newVal);
+                ///keySet() used for getting an key value
+            // }
+            // else
+            // {
+            //     map.put(ch,1);
+            // } 
+
+            
+
     }
-    return new HashSet<>(map.values()).size() == 1;
+   int value = map.getOrDefault(s.charAt(0), 0);  
+            for(char x:map.keySet()){
+                 if(!map.get(x).equals(value)){
+                    return false;
+                 }
+            } 
+    return true;
+    // return new HashSet<>(map.values()).size() == 1;
    }       
 }
+
+
