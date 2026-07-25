f92c508b570cf2fc03d6fe378d5892087f4cf668
POORNIMA J <poornijayaprakash5@gmail.com>
2026-07-25 13:36:50 +0530

    Time: 16 ms (20.81%), Space: 52.5 MB (85.49%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int threshold = nums.length / 2;
+        
+        for (int num : nums) {
+           
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            
+           
+            if (map.get(num) > threshold) {
+                return num;
+            }
+        }
+        
+        return -1; 
+    }
+}
