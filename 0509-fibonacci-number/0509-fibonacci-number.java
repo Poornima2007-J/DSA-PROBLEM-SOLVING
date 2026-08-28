f3b838cda2f900dd07f4103f0d4c28f34e52696a
POORNIMA J <poornijayaprakash5@gmail.com>
2026-08-28 21:46:56 +0530

    Time: 9 ms (42.84%), Space: 42.3 MB (15.98%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,8 @@
 class Solution {
     public int fib(int n) {
-        int a=0;
-        int b=1;
-        int c=0;
-        if(n<=1){
+        if(n<2){
             return n;
         }
-        for(int i=1;i<n;i++){
-            c=a+b;
-            a=b;
-            b=c;
-
-        }
-        return c;
+       return fib(n-1)+fib(n-2);
     }
 }
